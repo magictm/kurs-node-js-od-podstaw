138f4999c04d2b7a1636969c51678c6838c42c25
Marcin Stawowczyk <merlin@magictm.com>
2025-03-29 02:49:52 +0100

    changes

--- ignoreMyFile.js
+++
@@ -1,1 +0,0 @@
-console.loo('haha');
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// src/index.js
+
+console.log('Witaj w głównym projekcie kursu Node.js!');
+console.log("Środowisko przygotowane, plik uruchomiony z folderu 'src'.");
+
+const currentTime = new Date().toLocaleTimeString();
+console.log(`Aktualny czas serwera (przy uruchomieniu): ${currentTime}`);
