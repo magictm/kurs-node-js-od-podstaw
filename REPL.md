28a7984053d7b98ba0b2fc2769a89d512d7d3f14
Marcin Stawowczyk <merlin@magictm.com>
2025-03-29 01:44:15 +0100

    changes

---
+++ REPL.md
@@ -0,0 +1,41 @@
+# Pierwsze kroki z Node.js REPL
+
+```js
+2 + 2;
+
+10 * 5 - 3;
+
+//
+
+let imie = 'Ania';
+
+imie;
+
+//
+
+'Hello'.toUpperCase();
+
+Math.sqrt(9); // Pierwiastek kwadratowy z 9
+
+//
+
+console.log('To działa!');
+
+//
+
+let x = 10;
+
+if (x > 5) {
+    console.log('x jest większe niż 5');
+}
+
+//
+
+function dodaj(a, b) {
+    return a + b;
+}
+
+dodaj(2, 3);
+
+// .exit
+```
---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('Hej, tutaj Marcin Stawowczyk');
+
+// node app.js
+
+// console.log('Hej, tutaj Marcin 'm7rlin' Stawowczyk'); // Uncaught SyntaxError: missing ) after argument list
+// Uncaught SyntaxError: missing ) after argument list
+// console.log("Hej, tutaj Marcin "m7rlin" Stawowczyk"); // Uncaught SyntaxError: missing ) after argument list
+// Uncaught SyntaxError: missing ) after argument list
+
+console.log("Hej, tutaj Marcin 'm7rlin' Stawowczyk");
+// Hej, tutaj Marcin 'm7rlin' Stawowczyk
