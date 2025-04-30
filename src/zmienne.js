3f8e90627910e52566e8cb54471d046e36809d09
Marcin Stawowczyk <merlin@magictm.com>
2025-04-30 17:42:51 +0200

    changes

---
+++ src/zmienne.js
@@ -0,0 +1,25 @@
+// Deklaracja zmiennych różnych typów
+let liczba = 42;
+const tekst = 'Witaj, świecie!';
+let wartoscLogiczna = true;
+
+// Wyświetlenie wartości zmiennych
+console.log('Liczba:', liczba);
+console.log('Tekst:', tekst);
+console.log('Wartość logiczna:', wartoscLogiczna);
+
+// Proste operacje matematyczne
+let suma = liczba + 8;
+let roznica = liczba - 10;
+let iloczyn = liczba * 2;
+let iloraz = liczba / 2;
+
+console.log('Suma:', suma);
+console.log('Różnica:', roznica);
+console.log('Iloczyn:', iloczyn);
+console.log('Iloraz:', iloraz);
+
+// Połączenie dwóch zmiennych tekstowych
+let dodatkowyTekst = ' Jak się masz?';
+let polaczonyTekst = tekst + dodatkowyTekst;
+console.log('Połączony tekst:', polaczonyTekst);
