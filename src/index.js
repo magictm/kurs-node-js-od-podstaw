c58fb53e2fe79b8eee293543a9e37b7f43cf872c
Marcin Stawowczyk <merlin@magictm.com>
2025-04-30 17:39:58 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 // src/index.js
 
-console.log('Witaj w głównym projekcie kursu Node.js!');
-console.log("Środowisko przygotowane, plik uruchomiony z folderu 'src'.");
+// let
 
-const currentTime = new Date().toLocaleTimeString();
-console.log(`Aktualny czas serwera (przy uruchomieniu): ${currentTime}`);
+let wiek = 30; // Tworzymy zmienną o nazwie "wiek" i wkładamy do niej liczbę 30.
+console.log(wiek); // Wyświetli 30
+wiek = 31; // Zmieniamy wartość zmiennej "wiek" na 31.
+console.log(wiek); // Wyświetli 31
+
+// const
+
+const liczbaPi = 3.14;
+console.log(liczbaPi); // Wyświetli 3.14
+// liczbaPi = 3.1415;  // To spowoduje błąd! Nie można zmienić wartości stałej.
+
+const tworca = 'Merlin';
+console.log('Tworca aplikacji to: ' + tworca); // Wyświetli 'Tworca aplikacji to: Merlin'
+// tworca = 'Gandalf'; // To spowoduje błąd! Nie można zmienić wartości stałej.
+
+// var
+
+var liczba = 10; // Tworzymy zmienną o nazwie "liczba" i wkładamy do niej liczbę 10.
+console.log(liczba); // Wyświetli 10
+liczba = 20; // Zmieniamy wartość zmiennej "liczba" na 20.
+console.log(liczba); // Wyświetli 20
+
+// Nazwy zmiennych
+
+// Zmienne mogą mieć różne nazwy, ale muszą zaczynać się od litery, znaku podkreślenia (_) lub znaku dolara ($).
+// Nie mogą zaczynać się od cyfry.
+// let let = "a";
+let _let = 'b';
+let $let = 'c';
+let let1 = 'd';
