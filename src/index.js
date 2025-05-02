733b6692a8f6dd151c159e8fb2afd70aea8c0bf2
Marcin Stawowczyk <merlin@magictm.com>
2025-05-02 03:34:29 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-// src/index.js
+// // src/index.js
+// let liczba = 10;
+// let cena = 5.99;
+// let temperatura = -10.5;
+
+// let imie = 'Jan';
+// let nazwisko = 'Kowalski';
+// let miasto = `Warszawa`;
+
+// let czyAktywny = true;
+// let czyZalogowany = false;
+
+// let autor;
+// let autor2 = null;
+
+// console.log(autor);
+// console.log(typeof czyAktywny);
+
+// let gracz = {
+//     imie: 'Jan',
+//     nazwisko: 'Kowalski',
+//     wiek: 25,
+//     punkty: 100,
+// };
+
+// console.log(gracz.punkty);
+
+// let listaZakupow = ['chleb', 'mleko', 'jajka'];
+
+// console.log(listaZakupow[0]);
+// console.log(typeof listaZakupow);
+
+// const x = Infinity;
+// console.log(isNaN(NaN)); // false
+
+// console.log(9007199254740991 + 1); // 9007199254740992
+// console.log(9007199254740991 + 2); // 9007199254740992
+
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(bigInt);
+
+// prettier-ignore
+console.log(typeof(bigInt)); // bigint
